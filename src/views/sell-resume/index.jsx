d123dd620c24ae168d61b0b3dc81e619f00e2b30
Juan David Reyes <juandavidreyesbedoya@gmail.com>
2023-03-04 23:19:49 -0500

    some changes in the result page

--- src/views/sell-resume/index.jsx
+++ src/views/sell-resume/index.jsx
@@ -1,11 +1,13 @@
+import './main.css';
 export function SellResume() {
   return (
     <>
-    <div>HOLA!</div>
+    <div className="d-flex flex-column ibs-view">HOLA!
     <p>La venta fue registrada con exito!</p>
     <p>Aquí esta tu IBS</p>
     <div class="alert alert-primary" role="alert">
     12345456343
+    </div>
     </div>
     </>
   )
